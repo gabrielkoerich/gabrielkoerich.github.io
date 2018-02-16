dbb5be21a6f5a4e2f6a411f9904f0c0ccfe99ee5
Gabriel Koerich <gabriel@algorit.com.br>
2018-02-16 15:01:38 -0200

    hide

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -7,7 +7,7 @@ var pictures = [
   // 'https://en.gravatar.com/userimage/11625806/3a6df0444873d3eb52126312ac0ab901.jpg?size=600',
   'https://en.gravatar.com/userimage/11625806/3a6df0444873d3eb52126312ac0ab901.jpg?size=600',
   'https://en.gravatar.com/userimage/11625806/c29667c3f21dcc852dea802cbb0541d7.jpg?size=600',
-  'https://en.gravatar.com/userimage/11625806/ae6c42060c038da3a23e46f3877f1e65.jpg?size=600',
+  // 'https://en.gravatar.com/userimage/11625806/ae6c42060c038da3a23e46f3877f1e65.jpg?size=600',
   'http://graph.facebook.com/100000670444837/picture?width=400&amp;height=400',
   'https://cdn-images-1.medium.com/fit/c/200/200/0*x5dJKz8p5zDWgd0x.jpeg',
   // 'https://cdn-images-1.medium.com/fit/c/200/200/0*x5dJKz8p5zDWgd0x.jpeg',
